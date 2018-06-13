3fdd4189848cda7fabbc1bd7c0651530a3d59925
jsmask <2674800533@qq.com>
2018-06-13 16:22:49 +0800

    Update screend.js

--- screend.js
+++ screend.js
@@ -17,7 +17,7 @@
  */
 (function(global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-		typeof define === 'function' && define.amd ? define(factory) : (global.screend = factory());
+		typeof define === 'function' && define.amd ? define(factory) : (global.Screend = factory());
 }(this, (function() {
 
 	var isIE = document.documentMode,
